3b0860246e39e4a7b3c44b9c09828820f98fa28a
silverwolf1223 <101886723+silverwolf1223@users.noreply.github.com>
2026-04-24 13:47:19 -0500

    initialized routes

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+const signUpRouter = require('./routes/signUpRouter.js')
+const logInRouter = require('./routes/logInRouter.js')
+const userRouter = require('./rotues/userRouter.js')
+const chatRouter = require('./routes/chatRouter')
+
 app.use(express.urlencoded({extended: true}));
+
+app.use('/sign-up', signUpRouter);
+app.use('/log-in', logInRouter);
+app.user('/user/:id', userRouter);
+app.chatRouter('/chat/:id', chatRouter);
 
 
 const port = process.env.PORT || 3000;
